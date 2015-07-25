61bca61e423a7cbdb023986b8b35ffe725f8469f
Andrey Chabrov <idronium@gmail.com>
2015-07-25 14:21:52 +0300

    Add the new packages.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # using pip and virtualenv
-sudo apt-get -y install python-pip python-virtualenv python-dev libxml2-dev libxslt-dev libssl-dev
+sudo apt-get -y install python-pip python-virtualenv python-dev libxml2-dev libxslt-dev libssl-dev libffi-dev
 
 virtualenv --no-site-packages --python=python2.7 env
 env/bin/pip install -r requirements.txt
